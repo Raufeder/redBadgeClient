ef4430de103cc1e59f7ad8417d7415a1135b784d
Raufeder <you@example.com>
2020-12-07 19:07:10 -0500

    SPA routing

---
+++ src/helpers/environment.jsx
@@ -0,0 +1,14 @@
+let APIURL = "";
+
+switch (window.location.hostname) {
+  case "localhost" || "127.0.0.1":
+    APIURL = "http://localhost:8080/";
+    break;
+  case "dar-climbfinder.herokuapp.com":
+    APIURL = "https://climbfinder.herokuapp.com/";
+    break;
+    default:
+        
+}
+
+export default APIURL;
