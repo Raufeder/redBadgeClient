ff6046293587b305bc042e285ecbecb8b65ccc4d
Raufeder <you@example.com>
2021-10-29 23:45:52 -0400

    made my list

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,113 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
-import LoginComponent from './components/auth/Login';
-import RegisterComponent from './components/auth/Register';
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import LoginComponent from "./components/auth/Login";
+import RegisterComponent from "./components/auth/Register";
 import DeleteAccountComponent from "./components/auth/DeleteAccount";
-import NavbarComponent from './components/app/navbar';
-import HomePageComponent from './components/app/homePage';
-import RouteComponent from './components/app/routeList';
-import UserListComponent from './components/app/userList';
-import CreateRouteComponent from './components/app/addRoute';
+import NavbarComponent from "./components/app/navbar";
+import HomePageComponent from "./components/app/homePage";
+import RouteComponent from "./components/app/routeList";
+import UserListComponent from "./components/app/userList";
+import CreateRouteComponent from "./components/app/addRoute";
+
+//TODO Fix Navbar
+//TODO Add delete user button
+//TODO Add make admin button
+//TODO Make the app much better to look at
+//TODO Make cards much nicer
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [activeUsername, setActiveUsername] = useState('');
+  const [activeUsername, setActiveUsername] = useState("");
   const [activeId, setActiveId] = useState(0);
   const [activeRouteId, setActiveRouteId] = useState(null);
   const [admin, setAdmin] = useState(false);
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   const activeUser = (username) => {
-    setActiveUsername(username)
-    setIsLoggedIn(true)
-  }
+    setActiveUsername(username);
+    setIsLoggedIn(true);
+  };
 
   const activeUserId = (message) => {
-    setActiveId(message)
-  }
+    setActiveId(message);
+  };
 
   const loggedOut = (placeholder) => {
-    setIsLoggedIn(false)
-  }
+    setIsLoggedIn(false);
+  };
 
   const authenticateUser = (token) => {
     window.localStorage.setItem("authToken", token);
     setToken(token);
-  }
+  };
 
   return (
     <BrowserRouter>
-    <div className="App">
-        <NavbarComponent isLoggedIn={isLoggedIn} activeUsername={activeUsername} loggedOut={loggedOut} token={token} admin={admin}/>
+      <div className="App">
+        <NavbarComponent
+          isLoggedIn={isLoggedIn}
+          activeUsername={activeUsername}
+          loggedOut={loggedOut}
+          token={token}
+          admin={admin}
+        />
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <HomePageComponent />
           </Route>
           <Route exact path="/user/login">
-            <LoginComponent authenticateUser={authenticateUser} setActiveUsername={setActiveUsername} activeUser={activeUser} activeUserId={activeUserId} admin={admin} setAdmin={setAdmin}/>
+            <LoginComponent
+              authenticateUser={authenticateUser}
+              setActiveUsername={setActiveUsername}
+              activeUser={activeUser}
+              activeUserId={activeUserId}
+              admin={admin}
+              setAdmin={setAdmin}
+            />
           </Route>
           <Route exact path="/user/register">
-            <RegisterComponent setActiveUsername={setActiveUsername} setActiveId={setActiveId} activeUser={activeUser} activeUserId={activeUserId}/>
+            <RegisterComponent
+              setActiveUsername={setActiveUsername}
+              setActiveId={setActiveId}
+              activeUser={activeUser}
+              activeUserId={activeUserId}
+            />
           </Route>
           <Route exact path="/user/delete">
-              <DeleteAccountComponent loggedOut={loggedOut} token={token} />
+            <DeleteAccountComponent loggedOut={loggedOut} token={token} />
           </Route>
-          <Route exact path='/routes'>
-                <RouteComponent activeUsername={activeUsername} activeId={activeId} setActiveRouteId={setActiveRouteId} activeRouteId={activeRouteId} token={token} />
+          <Route exact path="/routes">
+            <RouteComponent
+              activeUsername={activeUsername}
+              activeId={activeId}
+              setActiveRouteId={setActiveRouteId}
+              activeRouteId={activeRouteId}
+              token={token}
+            />
           </Route>
-          <Route exact path='/admin/userlist'>
-                <UserListComponent activeUsername={activeUsername} activeId={activeId} setActiveRouteId={setActiveRouteId} activeRouteId={activeRouteId} token={token} />
+          <Route exact path="/admin/userlist">
+            <UserListComponent
+              activeUsername={activeUsername}
+              activeId={activeId}
+              setActiveRouteId={setActiveRouteId}
+              activeRouteId={activeRouteId}
+              token={token}
+            />
           </Route>
-          <Route exact path='/admin/add/route'>
-                <CreateRouteComponent activeUsername={activeUsername} activeId={activeId} setActiveRouteId={setActiveRouteId} activeRouteId={activeRouteId} token={token} />
+          <Route exact path="/admin/add/route">
+            <CreateRouteComponent
+              activeUsername={activeUsername}
+              activeId={activeId}
+              setActiveRouteId={setActiveRouteId}
+              activeRouteId={activeRouteId}
+              token={token}
+            />
           </Route>
         </Switch>
-    </div>
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
